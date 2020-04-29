03048d4a44848dc321bdd5ea24ca82908936f5c5
Hakim <cheboub.h@gmail.com>
2020-04-29 16:15:05 +0200

    hello world + color

---
+++ src/assets/js/script.js
@@ -0,0 +1,11 @@
+var h1 = document.createElement("h1");
+var content = document.createTextNode("Hello World !");
+h1.appendChild(content);
+document.body.appendChild(h1);
+h1.style.color = "red";
+
+
+
+
+
+
